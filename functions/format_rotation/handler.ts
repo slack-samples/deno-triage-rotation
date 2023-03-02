5edad192442348cfb36a444b94e680002a2c563c
Ashley Huynh <ashley.huynh@slack-corp.com>
2023-03-02 13:59:54 -0500

    Add in CI check for type checking

--- functions/format_rotation/handler.ts
+++ functions/format_rotation/handler.ts
@@ -2,9 +2,7 @@ import { SlackFunction } from "deno-slack-sdk/mod.ts";
 import { FormatRotationFunctionDefinition } from "./definition.ts";
 
 export default SlackFunction(FormatRotationFunctionDefinition, ({ inputs }) => {
-  const next_users = inputs.rotation.order.slice(1).map((a: string) =>
-    `<@${a}>`
-  ).join(
+  const next_users = inputs.rotation.order.slice(1).map((a) => `<@${a}>`).join(
     ", ",
   );
 
